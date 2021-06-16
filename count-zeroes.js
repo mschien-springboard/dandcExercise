04960b894b322ea2050cb100edc34c515d3e9009
Michael Schienbein <moomba0@gmail.com>
2021-06-16 18:01:23 +0700

    add sorted freq/update count zereos

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,43 +1,28 @@
-function sortedFrequency(arr, num) {
-    let firstIdx = findFirst(arr, num);
-    if (firstIdx == -1) return firstIdx;
-    let lastIdx = findLast(arr, num);
-    return lastIdx - firstIdx + 1;
+function countZeroes(arr) {
+    // add whatever parameters you deem necessary - good luck!
+    let firstZero = findFirst(arr)
+    if (firstZero === -1) return 0;
+    //  length of array - firstZero's index = number of 0s in a sorted array
+    return arr.length - firstZero
 }
 
-function findFirst(arr, num, leftIdx = 0, rightIdx = arr.length - 1) {
+function findFirst(arr, leftIdx = 0, rightIdx = arr.length - 1) {
+    // if left and right index havent crossed
     if (rightIdx >= leftIdx) {
-        let middleIdx = Math.floor((leftIdx + rightIdx) / 2)
-        // if the midpoint index is zero or num is greater than the value left of midpoint && the midpoint value is equal to num, then we've found the first occurance of num. this is the break point or "base case"
-        if ((middleIdx === 0 || num > arr[middleIdx - 1]) && arr[middleIdx] === num) {
+        let middleIdx = leftIdx + Math.floor((rightIdx - leftIdx) / 2)
+        // if midpoint value is 0 or left of mid point value is 1 && midpoint value is 0, return index of firstzero 
+        if ((middleIdx === 0 || arr[middleIdx - 1] === 1) && arr[middleIdx] === 0) {
             return middleIdx;
-            // if num is greater than value of midpoint, shift midpoint right by 1, then run function recursively.
-        } else if (num > arr[middleIdx]) {
-            return findFirst(arr, num, middleIdx + 1, rightIdx)
-            // else shift midpoint left by 1, then run function recursively
-        } else {
-            return findFirst(arr, num, leftIdx, middleIdx - 1)
+            // if midpoint value is one move index right by 1 and look for zero again recursively
+        } else if (arr[middleIdx] === 1) {
+            return findFirst(arr, middleIdx + 1, rightIdx)
         }
+        // move through left side recursively if zero is found
+        return findFirst(arr, leftIdx, middleIdx - 1)
     }
-    return -1
+    return -1;
 }
 
-function findLast(arr, num, leftIdx = 0, rightIdx = arr.length - 1) {
-    if (rightIdx >= leftIdx) {
-        let middleIdx = Math.floor((leftIdx + rightIdx) / 2)
-        // same as above but checks if midpoint is last value in arr or num is less than the value right of midpoint && value of mid point is num.
-        if ((middleIdx === arr.length - 1 || num < arr[middleIdx + 1]) && arr[middleIdx] === num) {
-            return middleIdx;
-            // shifts midpoint left by 1 if num is less than midpoint value, run recursively
-        } else if (num < arr[middleIdx]) {
-            return findLast(arr, num, leftIdx, middleIdx - 1)
-            // else shifts midpoint right, and run recursively
-        } else {
-            return findLast(arr, num, middleIdx + 1, rightIdx)
-        }
-    }
-    return -1
-}
 
 
 module.exports = countZeroes
